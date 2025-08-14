0f78ef481be81610d995fe5dadd436a152dc42ee
David Hobley <david.hobley@sharpblue.com.au>
2025-08-14 22:17:46 +1000

    provide sample scripts to run the web service

---
+++ calibrews.ps1
@@ -0,0 +1,8 @@
+#
+$env:DYNU_API = "eV66YVgfd54U32Y2g6T66YUVd473545U"
+$env:SSL_DIR = "ssl"
+$env:SSL_FULLCHAIN = "fullchain.crt"
+$env:SSL_PRIVATE_KEY = "private\sharpblue_org.key"
+$env:CALIBRE_LIBRARY = "."
+
+python calibrews.py
